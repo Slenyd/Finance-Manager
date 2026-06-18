a2ad35d42618d3fc7b4db2ad8715268a2bee299e
Raz Etedgi <razetedgi@gmail.com>
2026-06-18 20:39:34 +0300

    debug: add /api/debug-env Express route

--- backend/api/dbcheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const safe = (s) => s ? s.substring(0, 25) + '...' : undefined;
-  res.json({
-    POSTGRES_PRISMA_URL: safe(process.env.POSTGRES_PRISMA_URL),
-    DATABASE_URL: safe(process.env.DATABASE_URL),
-    POSTGRES_URL: safe(process.env.POSTGRES_URL),
-  });
-};
